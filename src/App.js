0f1b512c43b286ee25d9cc52c589a3fc55a977e6
YouJia Shhi <shi.shiyoujia@gmail.com>
2019-12-10 06:10:44 -0800

    removed nav

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Portfolio from './pages/Portfolio';
-import Register from './pages/Register';
-import Home from './pages/Home';
 import NotFound from './pages/NotFound';
 
 import './App.css';
@@ -11,10 +9,8 @@ function App() {
   return (
     <div className="App">
       <Router>
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/portfolio" component={Portfolio} />
-          <Route path="/register" component={Register} />
+        <Switch>   
+          <Route path="/" component={Portfolio} />
           <Route component={NotFound} />
         </Switch>
       </Router>
